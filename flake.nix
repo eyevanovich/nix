74ac6290ad863f50a652400ebc76de08611bcdb5
Ivan Miles Piesh <ipiesh@skysound.com>
2024-05-20 23:21:11 -0700

    blah

--- flake.nix
+++ flake.nix
@@ -3,16 +3,15 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
-    nixpkgs-darwin.url = "github:NixOS/nixpkgs/nixpkgs-23.11-darwin";
-
-    darwin.url = "github:LnL7/nix-darwin/master";
-    darwin.inputs.nixpkgs.follows = "nixpkgs-darwin";
-
     home-manager.url = "github:nix-community/home-manager/release-23.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    nixpkgs-darwin.url = "github:NixOS/nixpkgs/nixpkgs-23.11-darwin";
+    darwin.url = "github:LnL7/nix-darwin/master";
+    darwin.inputs.nixpkgs.follows = "nixpkgs-darwin";
   };
 
-  outputs = { self, nixpkgs, darwin, home-manager }: {
+  outputs = { self, nixpkgs, nixpkgs-darwin, darwin, home-manager }: {
     darwinConfigurations."hackbox2000" = darwin.lib.darwinSystem {
       system = "aarch64-darwin";
       modules = [
