43fe0e05f228ca0bf2cd7d82e0392ee6e1d2ad23
Ivan Miles Piesh <ipiesh@skysound.com>
2024-05-20 23:21:46 -0700

    blah

--- hosts/hackbox2000/homebrew.nix
+++ hosts/hackbox2000/homebrew.nix
@@ -1,17 +1,17 @@
 { ... }:
 
-homebrew = {
+{
   # Enable Homebrew
   # Note that enabling this option does not install Homebrew, see the Homebrew website for installation instructions.
   # https://brew.sh/
   # https://daiderd.com/nix-darwin/manual/index.html#opt-homebrew.enable
-  enable = true;
+  homebrew.enable = true;
 
   # Automatically use the Brewfile that this module generates in the Nix store
   # https://daiderd.com/nix-darwin/manual/index.html#opt-homebrew.global.brewfile
-  global.brewfile = true;
+  homebrew.global.brewfile = true;
 
-  taps = [
+  homebrew.taps = [
     "homebrew/cask-fonts"
     "homebrew/cask-versions"
     "homebrew/services"
@@ -20,7 +20,7 @@ homebrew = {
 
   # List of Homebrew formulae to install.
   # https://daiderd.com/nix-darwin/manual/index.html#opt-homebrew.brews
-  brews = [
+  homebrew.brews = [
     "awscli"
     "bat"
     "glow"
@@ -45,14 +45,14 @@ homebrew = {
   #
   # Commented apps suffer continual update issue:
   # https://github.com/malob/nixpkgs/issues/9
-  masApps = {
+  homebrew.masApps = {
     "Xcode" = 497799835;
   };
 
 
   # List of Homebrew casks to install.
   # https://daiderd.com/nix-darwin/manual/index.html#opt-homebrew.casks
-  casks = [
+  homebrew.casks = [
     "arc"
     "karabiner-elements"
     "obsidian"
