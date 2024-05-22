b9dff77735304128a3fe7704ca48b15c23181a3d
Ivan Miles Piesh <ipiesh@skysound.com>
2024-05-22 00:09:00 -0700

    apps

--- hosts/hackbox2000/modules/apps.nix
+++ hosts/hackbox2000/modules/apps.nix
@@ -1,56 +1,99 @@
 { pkgs, ... }:
 
 {
-  programs.fish.enable = true; 
 
   environment.variables.HOMEBREW_NO_ANALYTICS = "1";
-  environment.systemPackages = [ pkgs.neofetch ];
 
-  # Enable Homebrew
-  # Note that enabling this option does not install Homebrew, see the Homebrew website for installation instructions.
-  # https://brew.sh/
-  # https://daiderd.com/nix-darwin/manual/index.html#opt-homebrew.enable
-  homebrew.enable = true;
+  programs.fish.enable = true; 
 
-  # List of Homebrew formulae to install.
-  # https://daiderd.com/nix-darwin/manual/index.html#opt-homebrew.brews
-  homebrew.brews = [
-    "awscli"
-    "bat"
-    "glow"
-    "helix"
-    "bottom"
-    "jq"
-    "lazygit"
-    "zellij"
-    "marksman"
-    "neofetch"
-    "pre-commit"
-    "taplo"
-    "yq"
-    "fish"
-    "tldr"
-    "kubectx"
-    "direnv"
-    "yazi"
-  ];
-
-  # Prefer installing application from the Mac App Store
+  ##########################################################################
+  # 
+  #  Install all apps and packages here.
   #
-  # Commented apps suffer continual update issue:
-  # https://github.com/malob/nixpkgs/issues/9
-  # homebrew.masApps = {
-  #   "Xcode" = 497799835;
-  # };
+  #  NOTE: Your can find all available options in:
+  #    https://daiderd.com/nix-darwin/manual/index.html
+  # 
+  # TODO Fell free to modify this file to fit your needs.
+  #
+  ##########################################################################
 
-  # List of Homebrew casks to install.
-  # https://daiderd.com/nix-darwin/manual/index.html#opt-homebrew.casks
-  # homebrew.casks = [
-  #   "arc"
-  #   "karabiner-elements"
-  #   "obsidian"
-  #   "rectangle"
-  #   "spotify"
-  #   "setapp"
-  # ];
+  # Install packages from nix's official package repository.
+  #
+  # The packages installed here are available to all users, and are reproducible across machines, and are rollbackable.
+  # But on macOS, it's less stable than homebrew.
+  #
+  # Related Discussion: https://discourse.nixos.org/t/darwin-again/29331
+  environment.systemPackages = with pkgs; [
+    git
+    just # use Justfile to simplify nix-darwin's commands 
+  ];
+  environment.variables.EDITOR = "nvim";
+
+  # TODO To make this work, homebrew need to be installed manually, see https://brew.sh
+  # 
+  # The apps installed by homebrew are not managed by nix, and not reproducible!
+  # But on macOS, homebrew has a much larger selection of apps than nixpkgs, especially for GUI apps!
+  homebrew = {
+    enable = true;
+
+    onActivation = {
+      autoUpdate = false;
+      # 'zap': uninstalls all formulae(and related files) not listed here.
+      cleanup = "zap";
+    };
+
+    # Applications to install from Mac App Store using mas.
+    # You need to install all these Apps manually first so that your apple account have records for them.
+    # otherwise Apple Store will refuse to install them.
+    # For details, see https://github.com/mas-cli/mas 
+    masApps = {
+      # TODO Feel free to add your favorite apps here.
+
+      # Xcode = 497799835;
+      # Wechat = 836500024;
+      # NeteaseCloudMusic = 944848654;
+      # QQ = 451108668;
+      # WeCom = 1189898970;  # Wechat for Work
+      # TecentMetting = 1484048379;
+      # QQMusic = 595615424;
+    };
+
+    taps = [
+      "homebrew/cask-fonts"
+      "homebrew/services"
+      "homebrew/cask-versions"
+    ];
+
+    # `brew install`
+    brews = [
+      "awscli"
+      "bat"
+      "glow"
+      "helix"
+      "bottom"
+      "jq"
+      "lazygit"
+      "zellij"
+      "marksman"
+      "neofetch"
+      "pre-commit"
+      "taplo"
+      "yq"
+      "fish"
+      "tldr"
+      "kubectx"
+      "direnv"
+      "yazi"
+    ];
+
+    # `brew install --cask`
+    casks = [
+      "arc"
+      "karabiner-elements"
+      "obsidian"
+      "rectangle"
+      "spotify"
+      "setapp"
+    ];
+  };
 }
