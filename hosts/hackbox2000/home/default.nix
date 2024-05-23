5cde8cc5bba79c4de4aa12b4b183632005469cf6
Ivan Miles Piesh <ipiesh@skysound.com>
2024-05-22 22:41:54 -0700

    fish

---
+++ hosts/hackbox2000/home/default.nix
@@ -0,0 +1,43 @@
+{username, ...}: {
+  # import sub modules
+  imports = [
+    ./shell.nix
+    ./core.nix
+    ./git.nix
+    ./starship.nix
+  ];
+
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home = {
+    username = username;
+    homeDirectory = "/Users/${username}";
+
+    # This value determines the Home Manager release that your
+    # configuration is compatible with. This helps avoid breakage
+    # when a new Home Manager release introduces backwards
+    # incompatible changes.
+    #
+    # You can update Home Manager without changing this value. See
+    # the Home Manager release notes for a list of state version
+    # changes in each release.
+    stateVersion = "23.11";
+  };
+
+  # You can also manage environment variables but you will have to manually
+  # source
+  #
+  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  /etc/profiles/per-user/davish/etc/profile.d/hm-session-vars.sh
+  #
+  # if you don't want to manage your shell through Home Manager.
+  home.sessionVariables = {
+    EDITOR = "hx";
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
