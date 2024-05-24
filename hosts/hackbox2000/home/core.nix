6d105c49294a97e376cea56e06911da6008c08e4
Ivan Miles Piesh <ipiesh@skysound.com>
2024-05-24 10:04:08 -0700

    helix, wezterm, fish configs

--- hosts/hackbox2000/home/core.nix
+++ hosts/hackbox2000/home/core.nix
@@ -31,10 +31,6 @@
     fd # better find
     eza # better ls
 
-    # lsp
-    taplo
-    marksman
-
     # misc
     cowsay
     tree
@@ -43,6 +39,203 @@
   ];
 
   programs = {
+    helix = {
+      enable = true;
+      defaultEditor = true;
+      extraPackages = [
+        pkgs.marksman
+        pkgs.taplo
+        pkgs.typos-lsp
+      ];
+      settings = {
+        theme = "catppuccin_mocha";
+        ###############################################################################################
+        # EDITOR
+        ###############################################################################################
+        editor = {
+          true-color = true;
+          cursorline = true;
+          color-modes = true;
+          bufferline = "multiple";
+          scrolloff = 10;
+          gutters = ["diagnostics" "spacer" "line-numbers" "spacer" "diff"];
+        };
+        editor.whitespace.characters = {
+          space = "·";
+          nbsp = "⍽";
+          nnbsp = "␣";
+          tab = "→";
+          newline = "⏎";
+          tabpad = "·"; # Tabs will look like "→···" (depending on tab width)
+        };
+        editor.file-picker = {
+          hidden = false;
+        };
+        editor.cursor-shape = {
+          insert = "bar";
+          normal = "block";
+          select = "underline";
+        };
+        editor.lsp = {
+          display-messages = true;
+        };
+        editor.statusline = {
+          left = [
+            "mode"
+            "spacer"
+            "version-control"
+            "spacer"
+            "separator"
+            "file-name"
+            "file-modification-indicator"
+          ];
+          right = [
+            "spinner"
+            "spacer"
+            "workspace-diagnostics"
+            "separator"
+            "spacer"
+            "diagnostics"
+            "position"
+            "primary-selection-length"
+            "file-encoding"
+            "file-line-ending"
+            "file-type"
+          ];
+          separator = "│";
+          mode.normal = "NORMAL";
+          mode.insert = "INSERT";
+          mode.select = "SELECT";
+        };
+        editor.indent-guides = {
+          render = true;
+          character = "|";
+          skip-levels = 1;
+        };
+        editor.soft-wrap = {
+          enable = true;
+          max-wrap = 25; # increase value to reduce forced mid-word wrapping
+          max-indent-retain = 0;
+        };
+        ##############################################################################################3
+        # KEYs
+        ##############################################################################################3
+        keys.normal = {
+          C-j = ["extend_to_line_bounds" "delete_selection" "paste_after"];
+          C-k = ["extend_to_line_bounds" "delete_selection" "move_line_up" "paste_before"];
+          esc = ["collapse_selection" "keep_primary_selection"];
+          up = "no_op";
+          down = "no_op";
+          left = "no_op";
+          right = "no_op";
+          pageup = "no_op";
+          pagedown = "no_op";
+          home = "no_op";
+          end = "no_op";
+          "A-," = "goto_previous_buffer";
+          "A-." = "goto_next_buffer";
+          "A-w" = ":buffer-close";
+          "A-/" = "repeat_last_motion";
+          X = ["extend_line_up" "extend_to_line_bounds"];
+        };
+        keys.select = {
+          C-c = "toggle_comments";
+          up = "no_op";
+          down = "no_op";
+          left = "no_op";
+          right = "no_op";
+          pageup = "no_op";
+          pagedown = "no_op";
+          home = "no_op";
+          X = ["extend_line_up" "extend_to_line_bounds"];
+          end = "no_op";
+        };
+        keys.insert = {
+          C-c = "toggle_comments";
+          C-space = "completion";
+          up = "no_op";
+          down = "no_op";
+          left = "no_op";
+          right = "no_op";
+          pageup = "no_op";
+          pagedown = "no_op";
+          home = "no_op";
+          end = "no_op";
+        };
+        keys.normal.space.c = {
+          r = [":w" ":config-reload"];
+          o = ":config-open";
+          l = ":o ~/.config/helix/languages.toml";
+        };
+        keys.normal.space.o = {
+          g = ":sh zellij run -fc --height 100% --width 100% -x 0 -y 0 -- lazygit";
+          f = ":sh zellij run -fc -- yazi";
+          m = ":sh zellij run -fc -- glow -p ./README.md";
+          r = ":set whitespace.render none";
+          w = ":set whitespace.render all";
+        };
+        keys.normal.space.a = {
+          g = [":sh go mod tidy" ":reload-all" ":lsp-restart"];
+          r = [":reload-all" ":lsp-restart"];
+          l = ":sh zellij run -d right -- revive -formatter stylish ./...";
+        };
+        keys.normal.space = {
+          W = ":write";
+        };
+      };
+      languages.language = [
+        {
+          name = "elixir";
+          auto-format = true;
+        }
+      ];
+    };
+    wezterm = {
+      enable = true;
+      extraConfig = ''
+        -- Pull in the wezterm API
+        local wezterm = require 'wezterm'
+
+        -- This table will hold the configuration.
+        local config = {}
+
+        local mux = wezterm.mux
+        local act = wezterm.action
+
+        wezterm.on('gui-startup', function()
+          local tab, pane, window = mux.spawn_window({})
+          window:gui_window():maximize()
+        end)
+
+        if wezterm.config_builder then
+          config = wezterm.config_builder()
+        end
+
+        -- This is where you actually apply your config choices
+        config.color_scheme = 'Catppuccin Mocha'
+        config.font = wezterm.font 'MesloLGSDZ Nerd Font Mono'
+        config.font_size = 13
+        config.window_decorations = "RESIZE"
+        config.hide_tab_bar_if_only_one_tab = true
+        config.window_frame = {
+          font = wezterm.font { family = 'MesloLGSDZ Nerd Font Mono', weight = 'Regular' },
+        }
+        config.term = "wezterm"
+
+        config.keys = {
+          {
+            -- disable this because I constantly fat finger
+            -- tab and caps lock (CTRL) at the same time
+            key = 'Tab',
+            mods = 'CTRL',
+            action = wezterm.action.DisableDefaultAssignment,
+          },
+        }
+
+        -- and finally, return the configuration to wezterm
+        return config
+      '';
+    };
     fish = {
       enable = true;
       interactiveShellInit = ''
@@ -56,10 +249,10 @@
         fzf --fish | source
       '';
       shellAliases = {
-          "ls" = "ls -lAF";
-          mkdir = "mkdir -p";
-          ".." = "cd ..";
-          "..." = "cd ../..";
+        "ls" = "ls -lAF";
+        mkdir = "mkdir -p";
+        ".." = "cd ..";
+        "..." = "cd ../..";
       };
       shellAbbrs = {
         g = "git";
