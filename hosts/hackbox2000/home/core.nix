d31ba9a87b9c2c8a9482d36d90aa0718998ccc47
Ivan Miles Piesh <ipiesh@skysound.com>
2024-05-26 23:44:32 -0700

    fish git

--- hosts/hackbox2000/home/core.nix
+++ hosts/hackbox2000/home/core.nix
@@ -41,6 +41,56 @@
   ];
 
   programs = {
+    git = {
+      enable = true;
+      lfs.enable = true;
+
+      userName = "Ivan Miles Piesh";
+      userEmail = "ipiesh@skysound.com";
+
+      # includes = [
+      #   {
+      #     # use diffrent email & name for work
+      #     path = "~/work/.gitconfig";
+      #     condition = "gitdir:~/work/";
+      #   }
+      # ];
+
+      extraConfig = {
+        init.defaultBranch = "main";
+        push.autoSetupRemote = true;
+        pull.rebase = true;
+      };
+
+      # signing = {
+      #   key = "xxx";
+      #   signByDefault = true;
+      # };
+
+      delta = {
+        enable = true;
+        options = {
+          features = "side-by-side";
+        };
+      };
+
+      aliases = {
+        # common aliases
+        br = "branch";
+        co = "checkout";
+        st = "status";
+        ls = "log --pretty=format:\"%C(yellow)%h%Cred%d\\\\ %Creset%s%Cblue\\\\ [%cn]\" --decorate";
+        ll = "log --pretty=format:\"%C(yellow)%h%Cred%d\\\\ %Creset%s%Cblue\\\\ [%cn]\" --decorate --numstat";
+        cm = "commit -m";
+        ca = "commit -am";
+        dc = "diff --cached";
+        amend = "commit --amend -m";
+
+        # aliases for submodule
+        update = "submodule update --init --recursive";
+        foreach = "submodule foreach";
+      };
+    };
     eza = {
       enable = true;
       git = true;
@@ -318,6 +368,62 @@
         -- and finally, return the configuration to wezterm
         return config
       '';
+    };
+    fish = {
+      enable = true;
+      interactiveShellInit = ''
+        set fish_greeting # Disable greeting
+        export EDITOR="hx"
+        export GOPRIVATE="gitlab.disney.com/skywalker-sound/*,gitlab.disney.com/skywalker-sound/libraries/golang/*"
+
+        alias config='/usr/bin/git --git-dir=/Users/ipiesh/.cfg/.git/ --work-tree=/Users/ipiesh'
+
+        fish_add_path "/opt/homebrew/bin/"
+
+        ##Keep this at the end
+        fzf --fish | source
+      '';
+      shellAliases = {
+        "ls" = "ls -lAF";
+        mkdir = "mkdir -p";
+        ".." = "cd ..";
+        "..." = "cd ../..";
+      };
+      shellAbbrs = {
+        g = "git";
+        m = "make";
+        t = "task";
+        lg = "lazygit";
+        zj = "zellij";
+      };
+      plugins = [
+        {
+          name = "grc";
+          src = pkgs.fishPlugins.grc.src;
+        }
+        {
+          name = "tide";
+          src = pkgs.fishPlugins.tide.src;
+        }
+        {
+          name = "fzf";
+          src = pkgs.fishPlugins.fzf-fish.src;
+        }
+        {
+          name = "bass";
+          src = pkgs.fishPlugins.bass.src;
+        }
+        # Manual packaging and enable a plugin
+        # {
+        #   name = "z";
+        #   src = pkgs.fetchFromGitHub {
+        #     owner = "jethrokuan";
+        #     repo = "z";
+        #     rev = "e0e1b9dfdba362f8ab1ae8c1afc7ccf62b89f7eb";
+        #     sha256 = "0dbnir6jbwjpjalz14snzd3cgdysgcs3raznsijd6savad3qhijc";
+        #   };
+        # }
+      ];
     };
   };
 }
