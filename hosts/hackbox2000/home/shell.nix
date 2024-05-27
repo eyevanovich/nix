05d7e6121662eb73ab06c6d5ec17f571d838730c
Ivan Miles Piesh <ipiesh@skysound.com>
2024-05-26 23:32:02 -0700

    fish again

---
+++ hosts/hackbox2000/home/shell.nix
@@ -0,0 +1,58 @@
+{pkgs, ...}: {
+  programs.fish = {
+    enable = true;
+    interactiveShellInit = ''
+      set fish_greeting # Disable greeting
+      export EDITOR="hx"
+      export GOPRIVATE="gitlab.disney.com/skywalker-sound/*,gitlab.disney.com/skywalker-sound/libraries/golang/*"
+
+      alias config='/usr/bin/git --git-dir=/Users/ipiesh/.cfg/.git/ --work-tree=/Users/ipiesh'
+
+      fish_add_path "/opt/homebrew/bin/"
+
+      ##Keep this at the end
+      fzf --fish | source
+    '';
+    shellAliases = {
+      "ls" = "ls -lAF";
+      mkdir = "mkdir -p";
+      ".." = "cd ..";
+      "..." = "cd ../..";
+    };
+    shellAbbrs = {
+      g = "git";
+      m = "make";
+      t = "task";
+      lg = "lazygit";
+      zj = "zellij";
+    };
+    plugins = [
+      {
+        name = "grc";
+        src = pkgs.fishPlugins.grc.src;
+      }
+      {
+        name = "tide";
+        src = pkgs.fishPlugins.tide.src;
+      }
+      {
+        name = "fzf";
+        src = pkgs.fishPlugins.fzf-fish.src;
+      }
+      {
+        name = "bass";
+        src = pkgs.fishPlugins.bass.src;
+      }
+      # Manual packaging and enable a plugin
+      # {
+      #   name = "z";
+      #   src = pkgs.fetchFromGitHub {
+      #     owner = "jethrokuan";
+      #     repo = "z";
+      #     rev = "e0e1b9dfdba362f8ab1ae8c1afc7ccf62b89f7eb";
+      #     sha256 = "0dbnir6jbwjpjalz14snzd3cgdysgcs3raznsijd6savad3qhijc";
+      #   };
+      # }
+    ];
+  };
+}
