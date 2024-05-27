beb2af64ff3a804a0846bde5e2ecf0cbde60b2c8
Ivan Miles Piesh <ipiesh@skysound.com>
2024-05-26 22:29:51 -0700

    git and shell

--- hosts/hackbox2000/home/core.nix
+++ hosts/hackbox2000/home/core.nix
@@ -30,7 +30,6 @@
     fd # better find
     eza # better ls
     devbox
-    git-lfs
     go-task # taskfile
     zoxide # better cd
 
@@ -42,10 +41,11 @@
   ];
 
   programs = {
-    git = {
+    exa = {
       enable = true;
-      userName = "Ivan Miles Piesh";
-      userEmail = "ipiesh@skysound.com";
+      enableAliases = true;
+      git = true;
+      icons = true;
     };
     helix = {
       enable = true;
@@ -319,62 +319,6 @@
         -- and finally, return the configuration to wezterm
         return config
       '';
-    };
-    fish = {
-      enable = true;
-      interactiveShellInit = ''
-        set fish_greeting # Disable greeting
-        export EDITOR="hx"
-        export GOPRIVATE="gitlab.disney.com/skywalker-sound/*,gitlab.disney.com/skywalker-sound/libraries/golang/*"
-
-        alias config='/usr/bin/git --git-dir=/Users/ipiesh/.cfg/.git/ --work-tree=/Users/ipiesh'
-
-        fish_add_path "/opt/homebrew/bin/"
-
-        ##Keep this at the end
-        fzf --fish | source
-      '';
-      shellAliases = {
-        "ls" = "ls -lAF";
-        mkdir = "mkdir -p";
-        ".." = "cd ..";
-        "..." = "cd ../..";
-      };
-      shellAbbrs = {
-        g = "git";
-        m = "make";
-        j = "just";
-        lg = "lazygit";
-        zj = "zellij";
-      };
-      plugins = [
-        {
-          name = "grc";
-          src = pkgs.fishPlugins.grc.src;
-        }
-        {
-          name = "tide";
-          src = pkgs.fishPlugins.tide.src;
-        }
-        {
-          name = "fzf";
-          src = pkgs.fishPlugins.fzf-fish.src;
-        }
-        {
-          name = "bass";
-          src = pkgs.fishPlugins.bass.src;
-        }
-        # Manual packaging and enable a plugin
-        # {
-        #   name = "z";
-        #   src = pkgs.fetchFromGitHub {
-        #     owner = "jethrokuan";
-        #     repo = "z";
-        #     rev = "e0e1b9dfdba362f8ab1ae8c1afc7ccf62b89f7eb";
-        #     sha256 = "0dbnir6jbwjpjalz14snzd3cgdysgcs3raznsijd6savad3qhijc";
-        #   };
-        # }
-      ];
     };
   };
 }
