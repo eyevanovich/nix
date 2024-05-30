63ad312af0ee4c28348be5b0a9bd49a594971b6c
Ivan Miles Piesh <ipiesh@skysound.com>
2024-05-29 22:32:24 -0700

    flux and fix eza

--- hosts/hackbox2000/home/default.nix
+++ hosts/hackbox2000/home/default.nix
@@ -4,18 +4,8 @@
     ./core.nix
   ];
 
-  # Home Manager needs a bit of information about you and the
-  # paths it should manage.
+  # Home Manager needs a bit of information about you.
   home = {
-    # This value determines the home Manager release that your
-    # configuration is compatible with. This helps avoid breakage
-    # when a new home Manager release introduces backwards
-    # incompatible changes.
-    #
-    # You can update home Manager without changing this value. See
-    # the home Manager release notes for a list of state version
-    # changes in each release.
-    stateVersion = "23.11";
     username = username;
     homeDirectory = "/Users/${username}";
     sessionVariables = {
