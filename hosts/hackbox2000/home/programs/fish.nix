7bf5977d8598df8d150159d54ecd1d7ac47a4c72
Ivan Miles Piesh <ipiesh@skysound.com>
2024-06-12 16:00:09 -0700

    add wg-quick aliases to fishshell

--- hosts/hackbox2000/home/programs/fish.nix
+++ hosts/hackbox2000/home/programs/fish.nix
@@ -19,8 +19,8 @@
       mkdir = "mkdir -p";
       ".." = "cd ..";
       "..." = "cd ../..";
-      "w up" = "wg-quick up ~/.config/wireguard/wg0.conf";
-      "w down" = "wg-quick down ~/.config/wireguard/wg0.conf";
+      "wup" = "wg-quick up ~/.config/wireguard/wg0.conf";
+      "wdown" = "wg-quick down ~/.config/wireguard/wg0.conf";
     };
     shellAbbrs = {
       g = "git";
