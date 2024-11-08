ce43a4b99113b249eb985e5edb202d1740a0e483
Ivan Miles Piesh <ipiesh@skysound.com>
2024-11-07 21:56:33 -0800

    revert aerospace config

--- modules/home-manager/programs/aerospace.nix
+++
@@ -1,4 +0,0 @@
-{config, ...}: {
-  ## Make config mutable with mkOutOfStoreSymlink (note: needs absolute path to file)
-  home.file.".config/aerospace/aerospace.toml".source = config.lib.file.mkOutOfStoreSymlink "${config.xdg.configHome}/nix/dotfiles/aerospace/aerospace.toml";
-}
