631827df9865beb850799d0da52c83ebaab192b2
Ivan Miles Piesh <ipiesh@skysound.com>
2024-11-07 21:37:15 -0800

    borders again

--- modules/darwin/services/borders.nix
+++ modules/darwin/services/borders.nix
@@ -2,9 +2,9 @@
   # add jankyborders as it's being called within aerospace on startup
   services.jankyborders = {
     enable = true;
-    active_color = "0xff00ffff";
-    inactive_color = "0xffff0000";
-    width = 3.0;
+    active_color = "0xffffd700";
+    inactive_color = "0xff000000";
+    width = 8.0;
     hidpi = false;
   };
 }
