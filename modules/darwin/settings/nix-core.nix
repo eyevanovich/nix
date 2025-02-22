20a32feb11d5ff3660e67cdb8185d330447b1f3c
Ivan Miles Piesh <ipiesh@skysound.com>
2025-02-21 21:17:01 -0800

    remove nix.gc

--- modules/darwin/settings/nix-core.nix
+++ modules/darwin/settings/nix-core.nix
@@ -1,6 +1,5 @@
 {
   pkgs,
-  lib,
   ...
 }: {
   # enable flakes globally
@@ -13,12 +12,6 @@
   nix.enable = false;
 
   nix.package = pkgs.nix;
-
-  # do garbage collection weekly to keep disk usage low
-  nix.gc = {
-    automatic = lib.mkDefault true;
-    options = lib.mkDefault "--delete-older-than 7d";
-  };
 
   # Disable auto-optimise-store because of this issue:
   #   https://github.com/NixOS/nix/issues/7273
