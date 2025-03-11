d9f5af4f4cd095cde679bf69ef2f1fb3a63b481e
Ivan Miles Piesh <ipiesh@skysound.com>
2025-03-10 23:37:23 -0700

    multiple hosts

---
+++ hosts.nix
@@ -0,0 +1,18 @@
+# Host configurations
+{
+  "hackbox2000" = {
+    username = "ipiesh";
+    system = "aarch64-darwin";
+  };
+
+  "WFH-DEV-MBPX-PIESH" = {
+    username = "ipiesh";
+    system = "aarch64-darwin";
+  };
+
+  # Example of an Intel Mac
+  # "old-mac-mini" = {
+  #   username = "ipiesh";
+  #   system = "x86_64-darwin";
+  # };
+}
