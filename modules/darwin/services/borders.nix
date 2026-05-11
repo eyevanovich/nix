c229ec4316faf84252a8f00f2056946e78a65734
Ivan Miles Piesh <ipiesh@skysound.com>
2026-05-11 09:55:02 -0700

    fix: blacklist openwhispr for borders

--- modules/darwin/services/borders.nix
+++ modules/darwin/services/borders.nix
@@ -2,9 +2,10 @@
   # add jankyborders as it's being called within aerospace on startup
   services.jankyborders = {
     enable = true;
-    active_color = "0xffffd700";
+    active_color = "0xffff69b4";
     inactive_color = "0xff000000";
-    width = 3.0;
+    width = 5.0;
     hidpi = false;
+    blacklist = [ "OpenWhispr" ];
   };
 }
